89ce1685e9c6a8640b8416ee6341f95caa1419dd
Revatee Ghatage <ghatagerevati3@gmail.com>
2026-07-11 12:00:25 +0530

    login stuff

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const { sendOtp } = require("../controllers/authController");
 const register = require("../controllers/registerController");
+const login = require("../controllers/loginController");
 
 router.post("/otpsend", sendOtp);
 router.post("/register",register);
-
+router.post("/login",login)
 module.exports = router;
